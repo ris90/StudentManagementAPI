54f681670b453871234e23a6f129cf5f4db724c0
ris90 <116946053+ris90@users.noreply.github.com>
2022-11-06 07:26:37 -0600

    Add files via upload

---
+++ StudentManagementAPI/Startup.cs
@@ -0,0 +1,41 @@
+﻿using Newtonsoft.Json.Serialization;
+using StudentManagementAPI.Database;
+using StudentManagementAPI.Services;
+
+namespace StudentManagementAPI
+{
+    public class Startup
+    {
+        public static IConfiguration? Configuration { get; set; }
+        public static IWebHostEnvironment? Environment { get; set; }
+        public Startup(IConfiguration configuration, IWebHostEnvironment environment)
+        {
+            Configuration = configuration;
+            Environment = environment;
+        }
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSingleton<IMongoDbClient, MongoDbClient>();
+            services.AddSingleton<IStudentService, StudentService>();
+            services.AddCors(c =>
+            {
+                c.AddPolicy("AllowOrigin", options => options.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+            });
+            services.AddControllers();
+            services.AddEndpointsApiExplorer();
+
+        }
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseCors(options => options.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+            app.UseHttpsRedirection();
+            app.UseAuthentication();
+            app.UseRouting();
+            app.UseAuthorization();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
